52e025a45eb37443523d119ca42d26397a5dfca7
Gea-Suan Lin <gslin@kkbox.com>
2014-05-15 17:13:00 +0800

    Fix package name (garbd).

--- db3.sh
+++ db3.sh
@@ -17,7 +17,7 @@ EOF
     apt-get update
 
     DEBIAN_FRONTEND=noninteractive apt-get -y install dstat vim-nox
-    DEBIAN_FRONTEND=noninteractive apt-get -y install percona-xtradb-cluster-galera-2.x
+    DEBIAN_FRONTEND=noninteractive apt-get -y install percona-xtradb-cluster-garbd-2
 
     cat >> /etc/rc.local <<EOF
 /usr/bin/garbd -d -a gcomm://192.168.50.101 -g test -l /tmp/garbd-test.log
