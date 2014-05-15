30ddcc4ef73531866adb82f0357e7babd51190bb
Gea-Suan Lin <gslin@kkbox.com>
2014-05-15 17:16:49 +0800

    We need to specify port 4567 to connect to PXC.

--- db3.sh
+++ db3.sh
@@ -20,7 +20,7 @@ EOF
     DEBIAN_FRONTEND=noninteractive apt-get -y install percona-xtradb-cluster-garbd-2
 
     cat >> /etc/rc.local <<EOF
-/usr/bin/garbd -d -a gcomm://192.168.50.101 -g test -l /tmp/garbd-test.log
+/usr/bin/garbd -d -a gcomm://192.168.50.101:4567 -g test -l /tmp/garbd-test.log
 EOF
 }
 
